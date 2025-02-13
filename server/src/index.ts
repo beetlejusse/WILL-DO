55645b343412384e361931df576d3043fa620f50
beetlejusse <vishalbelwal52@gmail.com>
2025-02-13 23:41:49 +0530

    add server entry point with Express and MongoDB connection

---
+++ server/src/index.ts
@@ -0,0 +1,33 @@
+import { configDotenv } from 'dotenv';
+import express from 'express';
+import mongoose from 'mongoose';
+import authRoutes  from '../src/routes/auth.routes';
+import contentRoutes  from './routes/content.routes';
+import brainRoutes  from './routes/brain.routes';
+
+configDotenv();
+
+const app = express();
+app.use(express.json());
+
+app.use("/api/v1/auth", authRoutes)
+app.use("/api/v1/content", contentRoutes)
+app.use("/api/v1/brain", brainRoutes)
+
+async function connectDB() {
+    try {
+        const mongoUri = process.env.MONGO_URI;
+        if (!mongoUri) {
+            throw new Error("MONGO_URI is not defined in the environment variables");
+        }
+        await mongoose.connect(mongoUri);
+        app.listen(3000, () => {
+            console.log("Server is running on port 3000");
+        });
+        console.log("Connected to the database");
+    } catch (error: any) {
+        console.error(error.message);
+        process.exit(1);
+    }
+}
+connectDB();
